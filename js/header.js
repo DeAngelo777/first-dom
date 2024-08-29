be84b63c9652270f3aae8f100434e208896588cf
Laimonas <ldirgelas530@gmail.com>
2024-08-29 18:58:23 +0300

    up

--- js/header.js
+++ js/header.js
@@ -1,53 +1,53 @@
-export function header2(currentHref) {
-    const navData = [
-        {
-            href: 'first-dom',
-            text: 'Pagrindinis',
-        },
-        {
-            href: 'first-dom/services',
-            text: 'Paslaugos',
-        },
-        {
-            href: 'first-dom/team',
-            text: 'Komanda',
-        },
-        {
-            href: 'first-dom/basketball',
-            text: 'Krepšinis',
-        },
-    ];
+// export function header2(currentHref) {
+//     const navData = [
+//         {
+//             href: 'first-dom',
+//             text: 'Pagrindinis',
+//         },
+//         {
+//             href: 'first-dom/services',
+//             text: 'Paslaugos',
+//         },
+//         {
+//             href: 'first-dom/team',
+//             text: 'Komanda',
+//         },
+//         {
+//             href: 'first-dom/basketball',
+//             text: 'Krepšinis',
+//         },
+//     ];
 
-    let navHTML = '';
+//     let navHTML = '';
 
-    for (const link of navData) {
-        let classes = '';
+//     for (const link of navData) {
+//         let classes = '';
 
-        if (currentHref === link.href) {
-            classes = 'link active';
-        } else {
-            classes = 'link';
-        }
+//         if (currentHref === link.href) {
+//             classes = 'link active';
+//         } else {
+//             classes = 'link';
+//         }
 
-        navHTML += `<a class="${classes}" href="/${link.href}">${link.text}</a>`;
-    }
+//         navHTML += `<a class="${classes}" href="/${link.href}">${link.text}</a>`;
+//     }
 
-    const headerHTML = `
-        <header class="header">
-            <img class="logo" src="/first-dom/images/logo.png" alt="logo">
-            <nav class="nav">${navHTML}</nav>
-        </header>`;
+//     const headerHTML = `
+//         <header class="header">
+//             <img class="logo" src="/first-dom/images/logo.png" alt="logo">
+//             <nav class="nav">${navHTML}</nav>
+//         </header>`;
 
-    document.body.insertAdjacentHTML('afterbegin', headerHTML);
-}
+//     document.body.insertAdjacentHTML('afterbegin', headerHTML);
+// }
 
 export function header(currentHref) {
     const data = [
-        { href: '', text: 'Pagrindinis' },
-        { href: 'services', text: 'Paslaugos' },
-        { href: 'team', text: 'Komanda' },
-        { href: 'basketball', text: 'Krepšinis' },
-        { href: 'snake', text: 'Gyvatele' },
+        { href: 'first-dom', text: 'Pagrindinis' },
+        { href: 'first-dom/services', text: 'Paslaugos' },
+        { href: 'first-dom/team', text: 'Komanda' },
+        { href: 'first-dom/basketball', text: 'Krepšinis' },
+        { href: 'first-dom/snake', text: 'Gyvatele' },
 
 
     ];
@@ -61,7 +61,7 @@ export function header(currentHref) {
     }
 
     const HTML = `<header class="header">
-            <img class="logo" src="../images/logo.png" alt="logo">
+            <img class="logo" src="/first-dom/images/logo.png" alt="logo">
             <nav class="nav">${linkHTML.join('')}</nav>
         </header>`;
 
