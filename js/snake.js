d62f189f92574a3a31782eca5d28bd4c047c9cb3
Laimonas <ldirgelas530@gmail.com>
2024-08-29 15:27:50 +0300

    up

--- js/snake.js
+++ js/snake.js
@@ -1,86 +1,141 @@
 import { header } from './header.js';
-header('basketball');
+header('snake');
 
 const formDOM = document.forms[0];
-const [widthDOM, heightDOM] = formDOM.querySelectorAll('input'); // [{}, {}]
-const [widthNoteDOM, heightNoteDom] = formDOM.querySelectorAll('note'); // [{}, {}]
+const [widthDOM, heightDOM] = formDOM.querySelectorAll('input');
+const [widthNoteDOM, heightNoteDOM] = formDOM.querySelectorAll('.note');
 const worldDOM = document.getElementById('world');
-
-
 const cellSize = 48;
-const {clientHeight, clientWidth} = worldDOM;
 
-const width = Math.floor(clientWidth / cellSize);
-const height = Math.floor(clientHeight / cellSize);
-
-
-// uzpildome forma su paskaiciuota informacija pagal ekrano dydi
-widthDOM.value = width;
-widthDOM.max = width;
-widthNoteDOM.textContent = 'max plotis ' + width;
-
-
-heightDOM.value = height;
-heightDOM.max = height;
-heightNoteDom.textContent = 'max aukstis ' + height;
-
-let HTML = '';
-
-for (let y = 0; y < height; y++){
-let cellHTML;
-    for (let x = 0; x < width; x++){
-
-        cellHTML += `<div class="cell"></div>`;
-    } 
-
-}        HTML += `<div class="row">${cellHTML}</div>`;
-
-
-// Uzpildome pasauli langeliais
-worldDOM.innerHTML = `<div class="row">${'<div class="cell"> </div>'.repeat(width)}</div>`.repeat(height);
-
-
-
-const snakeDOM = document.querySelector('.snake');
-const chickenDOM = document.querySelector('.chicken');
-
+let snakeDOM = null;
+let chickenDOM = null;
 
 const snakePosition = {
     x: 0,
     y: 0,
-}
-
-snakeDOM.style.top = (snakePosition.y * cellSize + 'px');
-snakeDOM.style.left = (snakePosition.x * cellSize + 'px');
-
+};
 const chickenPosition = {
-    x: 0,
-    y: 0,
+    x: 2,
+    y: 2,
+};
+const world = {
+    width: 2,
+    height: 2,
+};
+
+function renderWorld() {
+    worldDOM.innerHTML = `<div class="row">${'<div class="cell"></div>'.repeat(world.width)}</div>`.repeat(world.height)
+        + '<img class="chicken" style="top: 96px; left: 96px;" src="./chicken.png" alt="Chicken">'
+        + '<img class="snake" style="top: 0px; left: 0px;" src="./snake-round.png" alt="Snake">';
+
+    snakeDOM = document.querySelector('.snake');
+    chickenDOM = document.querySelector('.chicken');
+
+    snakeDOM.style.top = (snakePosition.y * cellSize) + 'px';
+    snakeDOM.style.left = (snakePosition.x * cellSize) + 'px';
+
+    chickenDOM.style.top = (chickenPosition.y * cellSize) + 'px';
+    chickenDOM.style.left = (chickenPosition.x * cellSize) + 'px';
 }
 
-chickenDOM.style.top = (chickenPosition.y * cellSize + 'px');
-chickenDOM.style.left = (chickenPosition.x * cellSize + 'px');
+// function randomChickenPosition() {
+//     const newX = Math.floor(Math.random() * world.width);
+//     const newY = Math.floor(Math.random() * world.height);
 
+//     if (newX !== snakePosition.x && newY !== snakePosition.y) {
+//         chickenPosition.x = newX
+//         chickenPosition.y = newY;
+//     } else {
+//         randomChickenPosition();
+//     }
+// }
 
+function randomChickenPosition() {
+    let foundPair = false;
 
+    while (!foundPair) {
+        const newX = Math.floor(Math.random() * world.width);
+        const newY = Math.floor(Math.random() * world.height);
 
+        if (newX !== snakePosition.x || newY !== snakePosition.y) {
+            chickenPosition.x = newX
+            chickenPosition.y = newY;
+            foundPair = true;
+        }
+    }
 
+}
 
+function gameOfSnakeAndChicken() {
+    const { clientHeight, clientWidth } = worldDOM;
 
+    const width = Math.floor(clientWidth / cellSize);
+    const height = Math.floor(clientHeight / cellSize);
 
+    world.width = width;
+    world.height = height;
 
+    // uzpildome forma su perskaiciuota informacija pagal ekrano dydi
+    widthDOM.value = width;
+    widthDOM.max = width;
+    widthNoteDOM.textContent = 'max plotis: ' + width;
 
+    heightDOM.value = height;
+    heightDOM.max = height;
+    heightNoteDOM.textContent = 'max aukštis: ' + height;
 
+    snakePosition.x = Math.floor(world.width / 2);
+    snakePosition.y = Math.floor(world.height / 2);
 
+    randomChickenPosition();
+    renderWorld();
+}
 
-// const name = person.name;
-// const age = person.age;
+gameOfSnakeAndChicken();
 
-// const {name, age } = person;
+window.addEventListener('resize', gameOfSnakeAndChicken);
 
-// const marks = [10, 2];
+window.addEventListener('keyup', event => {
+    if (event.key === 'w') {
+        if (snakePosition.y > 0) {
+            snakePosition.y--;
+        }
+    }
+    if (event.key === 'a') {
+        if (snakePosition.x > 0) {
+            snakePosition.x--;
+        }
+    }
+    if (event.key === 's') {
+        if (snakePosition.y + 1 < world.height) {
+            snakePosition.y++;
+        }
+    }
+    if (event.key === 'd') {
+        if (snakePosition.x + 1 < world.width) {
+            snakePosition.x++;
+        }
+    }
 
-// const first = marks [0];
-// const second = marks[1];
+    snakeDOM.style.top = (snakePosition.y * cellSize) + 'px';
+    snakeDOM.style.left = (snakePosition.x * cellSize) + 'px';
 
-// const [first, second] = marks;
+    if (snakePosition.x === chickenPosition.x
+        && snakePosition.y === chickenPosition.y) {
+        console.log('PAGAVAU!!!');
+
+        randomChickenPosition();
+
+        chickenDOM.style.top = (chickenPosition.y * cellSize) + 'px';
+        chickenDOM.style.left = (chickenPosition.x * cellSize) + 'px';
+    }
+});
+
+formDOM.addEventListener('submit', event => {
+    event.preventDefault();
+
+    world.width = +widthDOM.value;
+    world.height = +heightDOM.value;
+
+    renderWorld();
+});
